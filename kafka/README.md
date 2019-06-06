606d87fdf9507f3354078406c970babc57cc67f2
P2883280 <C-MequannintMunye.Zeru@charter.com>
2019-06-06 11:34:01 -0500

    polishing the response

--- kafka/README.md
+++ kafka/README.md
@@ -6,5 +6,20 @@ $ zookeeper-server-start.bat ../../config/zookeeper.properties
 $ cd bin/windows
 $ kafka-server-start.bat ../../config/server.properties
 
+#To create Kafka topic "test"
+$ cd bin/windows
+$ kafka-topics.bat --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
+
+#To list down topics:
+$ kafka-topics.bat --list --bootstrap-server localhost:9092
+
+#To create and send a message on topic "test"
+$ kafka-console-producer.bat --broker-list localhost:9092 --topic test
+This is my message
+
+#To consume a message from the topic "test"
+$ kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic test --from-beginning
+This is my message
+
 #To test the POST method
 http://localhost:9000/kafka/publish/?message=test
--- kafka/src/main/java/com/charter/kafka/controller/KafkaController.java
+++ kafka/src/main/java/com/charter/kafka/controller/KafkaController.java
@@ -2,6 +2,8 @@ package com.charter.kafka.controller;
 
 import com.charter.kafka.engine.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,15 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/kafka")
 public class KafkaController {
 
-    private final Producer producer;
-
     @Autowired
-    KafkaController(Producer producer) {
-        this.producer = producer;
-    }
+    private Producer producer;
 
     @PostMapping(value = "/publish")
-    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+    public ResponseEntity<String> sendMessageToKafkaTopic(@RequestParam("message") String message) {
         this.producer.sendMessage(message);
+        return new ResponseEntity<>("\""+ message + "\"" + " sent to Kafka", HttpStatus.OK);
     }
 }
